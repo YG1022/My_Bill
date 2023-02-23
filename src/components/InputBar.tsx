dc2c2bb4caf07f1442de4badb24eaf58e131241f
YG1022 <958065532@qq.com>
2023-02-23 21:55:34 +0800

    chore: import scss and move css to styles

--- src/components/InputBar.tsx
+++ src/components/InputBar.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import './InputBar.css';
+import '../styles/InputBar.css';
 
 const InputBar = (): JSX.Element => {
     const assignAmount = () => {
         const numInput = document.querySelector('.number-input') as HTMLInputElement;
-        const billAmount = numInput.value;
+        const billAmount = numInput.value ? numInput.value : '0';
         numInput.value = '';
         const amountDisplay = document.querySelector('.amount-display') as HTMLElement;
         amountDisplay.innerHTML = billAmount;
@@ -12,14 +12,18 @@ const InputBar = (): JSX.Element => {
 
     return (
         <div>
-            <label htmlFor='Count'>Tap your count here: </label>
-            <input type='number' className='number-input' data-testid='input-content'
-                   placeholder='Only number is permitted!' />
+            <label htmlFor="Count">Tap your count here: </label>
+            <input
+                type="number"
+                className="number-input"
+                data-testid="input-content"
+                placeholder="Only number is permitted!"
+            />
             <button onClick={assignAmount}>Assign</button>
             <br />
-            <div data-testid='amount-content'>
+            <div data-testid="amount-content">
                 The amount of the money is $
-                <span className='amount-display' data-testid='amount'>
+                <span className="amount-display" data-testid="amount">
                     0
                 </span>
             </div>
