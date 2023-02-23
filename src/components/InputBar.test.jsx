f279b58ab21c00258391237e147dc483edf3bb0c
YG1022 <958065532@qq.com>
2023-02-23 14:12:42 +0800

    test: hp and sp with input

--- src/components/InputBar.test.jsx
+++ src/components/InputBar.test.jsx
@@ -19,4 +19,24 @@ describe('InputBar', () => {
             expect(amount).toBe('11');
         });
     });
+
+    it('should not render text if input character not number', async () => {
+        render(<InputBar />);
+        await userEvent.type(screen.getByTestId('input-content'), 'character');
+        await userEvent.click(screen.getByRole('button', { name: 'Assign' }));
+        const amount = screen.getByTestId('amount').innerHTML;
+        await waitFor(() => {
+            expect(amount).toBe('');
+        });
+    });
+
+    it('should render negative number', async () => {
+        render(<InputBar />);
+        await userEvent.type(screen.getByTestId('input-content'), '-13');
+        await userEvent.click(screen.getByRole('button', { name: 'Assign' }));
+        const amount = screen.getByTestId('amount').innerHTML;
+        await waitFor(() => {
+            expect(amount).toBe('-13');
+        });
+    });
 });
