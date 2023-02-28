44b388ac9f1067c996af597f7bbf1c27d0f113e9
YG1022 <958065532@qq.com>
2023-02-28 15:27:23 +0800

    feat: new site layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,57 @@
 import './App.scss';
-import { Layout, Space } from 'antd';
+import { DesktopOutlined, PieChartOutlined } from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { Breadcrumb, Layout, Menu } from 'antd';
 import AccountBook from './components/AccountBook/AccountBook';
-import React from 'react';
+import React, { useState } from 'react';
 
-const App = (): JSX.Element => {
-    const { Header, Footer, Content } = Layout;
+const { Sider, Header, Footer, Content } = Layout;
 
-    return (
-        <Space direction='vertical' style={{ width: '100%' }}>
-            <Layout>
-                <Header>My Bill</Header>
-                <Content>
-                    <AccountBook />
-                </Content>
-                <Footer>Saving is a virtue.</Footer>
-            </Layout>
-        </Space>
-    );
+type MenuItem = Required<MenuProps>['items'][number];
+
+function getItem(
+  label: React.ReactNode,
+  key: React.Key,
+  icon?: React.ReactNode,
+  children?: MenuItem[]
+): MenuItem {
+  return {
+    key,
+    icon,
+    children,
+    label,
+  } as MenuItem;
+}
+
+const items: MenuItem[] = [
+  getItem('Statistics data', '1', <PieChartOutlined />),
+  getItem('Operator', '2', <DesktopOutlined />),
+];
+
+const App: React.FC = () => {
+  const [collapsed, setCollapsed] = useState(false);
+
+  return (
+    <Layout style={{ minHeight: '100vh', width: '100%' }}>
+      <Sider collapsible collapsed={collapsed} onCollapse={value => setCollapsed(value)}>
+        <div className="search-holder" />
+        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
+      </Sider>
+      <Layout className="site-layout">
+        <Header className="site-header">My Bill</Header>
+        <Content className="site-content">
+          <Breadcrumb className="site-breadcrumb">
+            <Breadcrumb.Item>User</Breadcrumb.Item>
+            <Breadcrumb.Item>Bill</Breadcrumb.Item>
+          </Breadcrumb>
+          <div className="display-content">
+            <AccountBook />
+          </div>
+        </Content>
+        <Footer className="site-footer">Saving is a virtue.</Footer>
+      </Layout>
+    </Layout>
+  );
 };
 
 export default App;
