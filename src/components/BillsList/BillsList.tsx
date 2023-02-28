370e0ef6fd383b0b57cfe0b3b48e014b83a83c5e
YG1022 <958065532@qq.com>
2023-02-28 17:16:16 +0800

    chore: fix the height of Form

--- src/components/BillsList/BillsList.tsx
+++ src/components/BillsList/BillsList.tsx
@@ -18,13 +18,20 @@ const BillsList: React.FC<billsListProps> = ({ amountList, error }) => {
   const bills = amountList.map(item => item.amount);
 
   return (
-    <div style={{ height: 60 }}>
-      <Divider orientation="left">Bills List</Divider>
+    <div style={{ maxHeight: 500 }}>
+      <Divider orientation="left" style={{ height: 50 }}>
+        Bills List
+      </Divider>
       <span>The amount of all the bills is $</span>
       <span className="amount" data-testid="amount">
         {sumBill}
       </span>
-      <List bordered dataSource={bills} renderItem={bill => <List.Item>{bill}</List.Item>}></List>
+      <List
+        bordered
+        dataSource={bills}
+        renderItem={bill => <List.Item>{bill}</List.Item>}
+        style={{ maxHeight: 400 }}
+      ></List>
     </div>
   );
 };
