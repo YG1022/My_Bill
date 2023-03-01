adf8716fcc870bb9da70092717143fbf0bcf016c
YG1022 <958065532@qq.com>
2023-03-01 14:31:43 +0800

    refactor: convert hooks to useAccountBook

--- src/components/AccountBook/AccountBook.tsx
+++ src/components/AccountBook/AccountBook.tsx
@@ -1,30 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import './AccountBook.scss';
 import moment from 'moment';
-import { getBillItems } from './services/getBillItems';
+import {useAccountBook} from './hooks/useAccountBook';
 import InputBar from '../InputBar/InputBar';
 import BillsList from '../BillsList/BillsList';
-import { PostgrestError } from '@supabase/supabase-js';
 
 const dateStamp: string = moment().format('YYYY-MM-DD HH:mm:ss');
 
 const AccountBook: React.FC = () => {
-    const [error, setError] = useState<PostgrestError>(null);
-    const [amountList, setAmountList] = useState([]);
-
-    const fetchData = async () => {
-        const { data, error: sqlError } = await getBillItems();
-
-        if (sqlError) {
-            setError(sqlError);
-            setAmountList([]);
-            console.log(error);
-        }
-        if (data) {
-            setAmountList(data);
-            setError(null);
-        }
-    };
+    const {amountList, error, fetchData} = useAccountBook();
 
     useEffect(() => {
         fetchData();
--- src/components/AccountBook/hooks/useAccountBook.ts
+++ src/components/AccountBook/hooks/useAccountBook.ts
@@ -1,3 +1,26 @@
-const useAccountBook = () => {};
+import { useState } from 'react';
+import { PostgrestError } from '@supabase/supabase-js';
+import { getBillItems } from '../services/getBillItems';
+
+const useAccountBook = () => {
+    const [error, setError] = useState<PostgrestError>(null);
+    const [amountList, setAmountList] = useState([]);
+
+    const fetchData = async () => {
+        const { data, error: sqlError } = await getBillItems();
+
+        if (sqlError) {
+            setError(sqlError);
+            setAmountList([]);
+            console.log(error);
+        }
+        if (data) {
+            setAmountList(data);
+            setError(null);
+        }
+    };
+
+    return { error, amountList, fetchData };
+};
 
 export { useAccountBook };
