67c7cc87c6fef02c79a809ed487d56d5cd31d3ef
YG1022 <958065532@qq.com>
2023-03-02 11:32:02 +0800

    feat: modify the style of input content

--- src/pages/InputAmount/InputAmount.tsx
+++ src/pages/InputAmount/InputAmount.tsx
@@ -1,34 +1,28 @@
 import { Button, Form, Input } from 'antd';
 import React from 'react';
 import { useInputAmount } from './hooks/useInputAmount';
+import './InputAmount.scss';
 
 const InputAmount: React.FC = () => {
   const [form] = Form.useForm();
   const { layout, tailLayout, onFinish } = useInputAmount(form);
 
   return (
-    <div style={{ height: 100 }}>
-      <Form
-        {...layout}
-        form={form}
-        name="control-hooks"
-        onFinish={onFinish}
-        style={{ maxWidth: 600 }}
+    <Form className="input-amount" {...layout} form={form} name="control-hooks" onFinish={onFinish}>
+      <Form.Item
+        className="input-content"
+        name="amount"
+        label="Amount"
+        rules={[{ required: true, message: 'Please input number!' }]}
       >
-        <Form.Item
-          name="amount"
-          label="Amount"
-          rules={[{ required: true, message: 'Please input number!' }]}
-        >
-          <Input type="number" />
-        </Form.Item>
-        <Form.Item {...tailLayout}>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
-    </div>
+        <Input type="number" />
+      </Form.Item>
+      <Form.Item {...tailLayout}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
   );
 };
 
