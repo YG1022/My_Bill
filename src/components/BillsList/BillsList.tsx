3b0eb34c4215d81ba7e88ef598edc5b30aefd9b8
YG1022 <958065532@qq.com>
2023-03-02 14:18:59 +0800

    style: justify bills list style

--- src/components/BillsList/BillsList.tsx
+++ src/components/BillsList/BillsList.tsx
@@ -2,24 +2,27 @@ import { Divider, List } from 'antd';
 import React from 'react';
 import { billsListProps } from '../../constants/types';
 import { useBillsList } from './hooks/useBillsList';
+import './BillsList.scss';
 
 const BillsList: React.FC<billsListProps> = ({ amountList }) => {
   const { bills, sumBill } = useBillsList(amountList);
 
   return (
-    <div style={{ maxHeight: 500 }}>
-      <Divider orientation="left" style={{ height: 50 }}>
+    <div className='bills-list'>
+      <Divider className='bills-divider' orientation='left'>
         Bills List
       </Divider>
-      <span>The amount of all the bills is $</span>
-      <span className="amount" data-testid="amount">
+      <div className='amount-content'>
+        <span>The amount of all the bills is $</span>
+        <span className='amount' data-testid='amount'>
         {sumBill}
       </span>
+      </div>
       <List
+        className='list-content'
         bordered
         dataSource={bills}
         renderItem={bill => <List.Item>{bill}</List.Item>}
-        style={{ maxHeight: 400, overflow: 'scroll' }}
       ></List>
     </div>
   );
