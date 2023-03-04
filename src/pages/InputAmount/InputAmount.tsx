99ec70a9ed9793827c384b5f640344d082f9b8d0
YG1022 <958065532@qq.com>
2023-03-04 18:19:30 +0800

    chore: move style of input and select bar to scss

--- src/pages/InputAmount/InputAmount.tsx
+++ src/pages/InputAmount/InputAmount.tsx
@@ -10,25 +10,26 @@ const InputAmount: React.FC = () => {
   return (
     <Form className="input-amount" {...layout} form={form} name="control-hooks" onFinish={onFinish}>
       <Form.Item
-        className="input-content"
         name="amount"
         label="Amount"
         rules={[{ required: true, message: 'Please input number!' }]}
       >
-        <InputNumber placeholder="Please input number" style={{ width: 300 }} />
+        <InputNumber className="input-bar" placeholder="Please input number" />
       </Form.Item>
-      <Form.Item name="category" label="Category" rules={[{ required: true }]}>
-        <Select style={{ width: 300 }}>
+      <Form.Item
+        name="category"
+        label="Category"
+        rules={[{ required: true, message: 'Please select category!' }]}
+      >
+        <Select className="select-bar">
           <Select.Option value="+">Income</Select.Option>
           <Select.Option value="-">Expenses</Select.Option>
         </Select>
       </Form.Item>
       <Form.Item {...tailLayout} shouldUpdate>
-        {() => (
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        )}
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
       </Form.Item>
     </Form>
   );
