5329a539a6f890acaef9116746a89eb7f0b771d2
YG1022 <958065532@qq.com>
2023-03-09 10:56:13 +0800

    fix: do not treat number in url in advance

--- src/components/CustomBreadCrumb/hooks/useCustomBreadCrumb.tsx
+++ src/components/CustomBreadCrumb/hooks/useCustomBreadCrumb.tsx
@@ -2,11 +2,10 @@ import { useLocation } from 'react-router-dom';
 import { ROUTES_PATH_NAME } from '../../../constants/routes';
 
 const useCustomBreadCrumb = () => {
-  const numberReg = /^\d+$/;
   const location = useLocation();
+
   const routes = location.pathname
     .split('/')
-    .filter((path) => !numberReg.test(path))
     .map((path) => ({
       path,
       breadcrumbName: ROUTES_PATH_NAME[path] || path,
