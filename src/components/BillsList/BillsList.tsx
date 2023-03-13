164351bc1d6c0574be40ec8dfd3163e13faaa9d6
YG1022 <958065532@qq.com>
2023-03-13 17:55:51 +0800

    feat: render amount and tags of per transaction

--- src/components/BillsList/BillsList.tsx
+++ src/components/BillsList/BillsList.tsx
@@ -8,21 +8,21 @@ const BillsList: React.FC<billsListProps> = ({ amountList, category }) => {
   const { transactions, totalAmount } = useBillsList(amountList, category);
 
   return (
-    <div className="bills-list">
-      <Divider className="bills-divider" orientation="left">
+    <div className='bills-list'>
+      <Divider className='bills-divider' orientation='left'>
         Transactions List
       </Divider>
-      <div className="amount-content">
+      <div className='amount-content'>
         <span>The amount of all the bills is $</span>
-        <span className="amount" data-testid="amount">
+        <span className='amount' data-testid='amount'>
           {totalAmount}
         </span>
       </div>
       <List
-        className="list-content"
+        className='list-content'
         bordered
         dataSource={transactions}
-        renderItem={transaction => <List.Item>{transaction}</List.Item>}
+        renderItem={transaction => <List.Item>{transaction.amount} - {transaction.tags}</List.Item>}
       ></List>
     </div>
   );
--- src/components/BillsList/hooks/useBillsList.ts
+++ src/components/BillsList/hooks/useBillsList.ts
@@ -6,9 +6,8 @@ const useBillsList = (amountList: Array<fetchedBillItem>, category: string) => {
     .filter(item => item.category === category)
     .reduce((total, curr) => total + Number(curr.amount), 0);
 
-  const transactions: Array<string> = amountList
-    .filter(item => item.category === category)
-    .map(item => item.amount);
+  const transactions: Array<fetchedBillItem> = amountList
+    .filter(item => item.category === category);
 
   return { totalAmount, transactions };
 };
