c2f57b2c66dd9bff815f896ef2cbb89b9004310f
YG1022 <958065532@qq.com>
2023-03-13 22:55:21 +0800

    feat: display tags in tag style

--- src/components/BillsList/BillsList.tsx
+++ src/components/BillsList/BillsList.tsx
@@ -1,4 +1,4 @@
-import { Divider, List } from 'antd';
+import { Divider, List, Tag } from 'antd';
 import React from 'react';
 import { billsListProps } from '../../constants/types';
 import { useBillsList } from './hooks/useBillsList';
@@ -8,21 +8,28 @@ const BillsList: React.FC<billsListProps> = ({ amountList, category }) => {
   const { transactions, totalAmount } = useBillsList(amountList, category);
 
   return (
-    <div className='bills-list'>
-      <Divider className='bills-divider' orientation='left'>
+    <div className="bills-list">
+      <Divider className="bills-divider" orientation="left">
         Transactions List
       </Divider>
-      <div className='amount-content'>
+      <div className="amount-content">
         <span>The amount of all the bills is $</span>
-        <span className='amount' data-testid='amount'>
+        <span className="amount" data-testid="amount">
           {totalAmount}
         </span>
       </div>
       <List
-        className='list-content'
+        className="list-content"
         bordered
         dataSource={transactions}
-        renderItem={transaction => <List.Item>{transaction.amount} - {transaction.tags}</List.Item>}
+        renderItem={transaction => (
+          <List.Item>
+            {transaction.amount}
+            {transaction.tags.split(',').map((tag, index) => (
+              <Tag key={index}>{tag}</Tag>
+            ))}
+          </List.Item>
+        )}
       ></List>
     </div>
   );
