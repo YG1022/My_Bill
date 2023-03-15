83f9c45803f6aea9d7de7a4b61ab5142c3bc706b
YG1022 <958065532@qq.com>
2023-03-15 14:09:25 +0800

    test: should render AccountBook correctly

--- src/pages/AccountBook/AccountBook.test.tsx
+++ src/pages/AccountBook/AccountBook.test.tsx
@@ -1,42 +1,51 @@
 import { screen, render, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import ShallowRenderer from 'react-test-renderer/shallow';
 import React from 'react';
 import AccountBook from './AccountBook';
+import { PageContainer } from '../../components/PageContainer/PageContainer';
 
 describe('AccountBook', () => {
-    it('should show AccountBook correctly', () => {
-        render(<AccountBook />);
-        const amountContent = screen.getByTestId('amount-content');
-        expect(amountContent).toBeInTheDocument();
-    });
+  it('should render AccountBook correctly', () => {
+    // Arrange
+    const renderer = new ShallowRenderer();
+    renderer.render(<AccountBook />);
 
-    it('should render text input on screen', async () => {
-        render(<AccountBook />);
-        await userEvent.type(screen.getByTestId('input-bar'), '11');
-        await userEvent.click(screen.getByRole('button', { name: 'Assign' }));
-        const amount = screen.getByTestId('amount').innerHTML;
-        await waitFor(() => {
-            expect(amount).toBe('11');
-        });
-    });
+    // Act
+    const result = renderer.getRenderOutput();
 
-    it('should render 0 if input character not number', async () => {
-        render(<AccountBook />);
-        await userEvent.type(screen.getByTestId('input-bar'), 'character');
-        await userEvent.click(screen.getByRole('button', { name: 'Assign' }));
-        const amount = screen.getByTestId('amount').innerHTML;
-        await waitFor(() => {
-            expect(amount).toBe('0');
-        });
-    });
+    // Assert
+    expect(result.type).toEqual(PageContainer);
+    expect(result.props.children.props.children.length).toBe(2);
+  });
 
-    it('should render negative number', async () => {
-        render(<AccountBook />);
-        await userEvent.type(screen.getByTestId('input-bar'), '-13');
-        await userEvent.click(screen.getByRole('button', { name: 'Assign' }));
-        const amount = screen.getByTestId('amount').innerHTML;
-        await waitFor(() => {
-            expect(amount).toBe('-13');
-        });
+  it('should render text input on screen', async () => {
+    render(<AccountBook />);
+    await userEvent.type(screen.getByTestId('input-bar'), '11');
+    await userEvent.click(screen.getByRole('button', { name: 'Assign' }));
+    const amount = screen.getByTestId('amount').innerHTML;
+    await waitFor(() => {
+      expect(amount).toBe('11');
     });
+  });
+
+  it('should render 0 if input character not number', async () => {
+    render(<AccountBook />);
+    await userEvent.type(screen.getByTestId('input-bar'), 'character');
+    await userEvent.click(screen.getByRole('button', { name: 'Assign' }));
+    const amount = screen.getByTestId('amount').innerHTML;
+    await waitFor(() => {
+      expect(amount).toBe('0');
+    });
+  });
+
+  it('should render negative number', async () => {
+    render(<AccountBook />);
+    await userEvent.type(screen.getByTestId('input-bar'), '-13');
+    await userEvent.click(screen.getByRole('button', { name: 'Assign' }));
+    const amount = screen.getByTestId('amount').innerHTML;
+    await waitFor(() => {
+      expect(amount).toBe('-13');
+    });
+  });
 });
