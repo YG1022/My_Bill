dafa2386f3daa354762c7f619e2f739116072d43
YG1022 <958065532@qq.com>
2023-03-18 23:55:07 +0800

    test: should add a transaction item to supabase

---
+++ src/services/addBillItem.test.ts
@@ -0,0 +1,34 @@
+import { waitFor } from '@testing-library/react';
+import { supabaseClient } from '../supabaseClient';
+import { addBillItem } from './addBillItem';
+
+const mockResultData = [{
+  data: [{ id: 1, date: '2023-03-17', amount: 100, category: '+', tags: ['tag 1'] }],
+  error: null,
+}];
+
+jest.mock('../supabaseClient');
+const mockedSupabaseClient = supabaseClient as jest.Mocked<typeof supabaseClient>;
+
+describe('add transaction items', () => {
+  const fromMock = mockedSupabaseClient.from as any;
+
+  beforeEach(() => {
+    fromMock.mockReturnValue({
+      insert: jest.fn().mockReturnValue({
+        select: jest.fn().mockReturnValue(mockResultData),
+      }),
+    });
+  });
+
+  it('should add a transaction item to supabase', async () => {
+    // Arrange
+
+    // Act
+
+    // Assert
+    await waitFor(async () => {
+      return expect(await addBillItem({ tags: '' })).toEqual(mockResultData);
+    });
+  });
+});
