8166b35d6d6eb2936fbfca4c9ccdcbaa57918c7d
YG1022 <958065532@qq.com>
2023-03-18 23:49:20 +0800

    test: should get bill items

--- src/services/getBillItems.test.ts
+++ src/services/getBillItems.test.ts
@@ -1,32 +1,32 @@
-import { getBillItems } from './getBillItems';
+import { waitFor } from '@testing-library/react';
 import { supabaseClient } from '../supabaseClient';
-import { act } from 'react-dom/test-utils';
+import { getBillItems } from './getBillItems';
+
+const mockResultData = [{
+  data: [{ id: 1, date: '2023-03-17', amount: 100, category: '+', tags: ['tag 1'] }],
+  error: null,
+}];
 
 jest.mock('../supabaseClient');
-const mockedSupabase = supabaseClient as jest.Mocked<typeof supabaseClient>;
+const mockedSupabaseClient = supabaseClient as jest.Mocked<typeof supabaseClient>;
 
-describe('getBillItems from supacase', () => {
-  const mockedPostgresQueryBuilder = {
-    url: new URL('http://localhost:3000'),
-    select: jest.fn().mockResolvedValueOnce({
-      data: [{ id: 1, name: 'test', amount: 1, date: '2021-01-01' }],
-      error: null,
-    }),
-    headers: {},
-    insert: jest.fn(),
-    update: jest.fn(),
-    delete: jest.fn(),
-    upsert: jest.fn(),
-  };
+describe('getBillItems from supabase', () => {
+  const fromMock = mockedSupabaseClient.from as any;
+
+  beforeEach(() => {
+    fromMock.mockReturnValue({
+      select: jest.fn().mockReturnValue(mockResultData),
+    });
+  });
 
   it('should get bill items', async () => {
     // Arrange
-    await act(() => {
-      mockedSupabase.from.mockReturnValueOnce(mockedPostgresQueryBuilder);
-    });
+
     // Act
 
     // Assert
-    expect(await getBillItems()).not.toBeNull();
+    await waitFor(async () => {
+      return expect(await getBillItems()).toEqual( mockResultData );
+    });
   });
 });
