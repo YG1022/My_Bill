a7719fe79d1bf7a178686f5b70b3f495a2a559e4
YG1022 <958065532@qq.com>
2023-03-20 10:13:19 +0800

    test: should show message when no number input

--- src/pages/InputAmount/InputAmount.test.tsx
+++ src/pages/InputAmount/InputAmount.test.tsx
@@ -3,6 +3,9 @@ import InputAmount from './InputAmount';
 import ShallowRenderer from 'react-test-renderer/shallow';
 import React from 'react';
 import { Form } from 'antd/lib';
+import { render, screen, act } from '../../utils/custom-testing-library';
+import userEvent from '@testing-library/user-event';
+import { waitFor } from '@testing-library/react';
 
 jest.mock('./hooks/useInputAmount');
 const mockedUseInputAmount = useInputAmount as jest.MockedFunction<typeof useInputAmount>;
@@ -25,5 +28,20 @@ describe('InputAmount', () => {
     // Assert
     expect(view.type).toEqual(Form);
     expect(view.props.children.length).toBe(4);
+  });
+
+  it('should show message when no number input', async () => {
+    // Arrange
+    render(<InputAmount />);
+    // Act
+    await act(async () => {
+      await userEvent.type(screen.getByRole('spinbutton', { name: 'Amount' }), 'test 1');
+      await userEvent.click(screen.getByRole('button', { name: 'Submit' }));
+    });
+    // Assert
+    await waitFor(() => {
+      expect(screen.getByText('Please input number!')).toBeInTheDocument();
+      expect(screen.getByText('Please select category!')).toBeInTheDocument();
+    });
   });
 });
