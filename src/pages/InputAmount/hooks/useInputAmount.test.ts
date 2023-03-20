73c6cfce2d03b86518b2ab28f236f61a29b21e10
YG1022 <958065532@qq.com>
2023-03-20 14:15:50 +0800

    test: should return data correctly

--- src/pages/InputAmount/hooks/useInputAmount.test.ts
+++ src/pages/InputAmount/hooks/useInputAmount.test.ts
@@ -6,30 +6,45 @@ jest.mock('../../../services/addBillItem');
 const mockedAddBillItem = addBillItem as jest.MockedFunction<typeof addBillItem>;
 
 describe('useInputAmount', () => {
-  // const mockedResolvedData = {
-  //   count: null,
-  //   data: [{ id: 1, amount: 100, date: '2021-01-01', category: 'income', tags: ['Food'] }],
-  //   error: null,
-  //   status: 200,
-  //   statusText: '',
-  // };
-  const mockedResolvedData = {
+  const mockedResolvedError = {
     count: null,
     data: null,
-    error: 'Sync Error',
+    error: 'Sync Error is occured!',
+    status: 200,
+    statusText: '',
+  };const mockedResolvedData = {
+    count: null,
+    data: [{ id: 1, amount: 100, date: '2021-01-01', category: 'income', tags: ['Food'] }],
+    error: null,
     status: 200,
     statusText: '',
   };
-  const mockedForm = jest.fn().mockReturnValue({ resetFields: jest.fn() });
+  const mockedForm = {
+    resetFields: jest.fn(),
+  };
 
   beforeEach(() => {
     mockedAddBillItem.mockClear();
-    mockedAddBillItem.mockResolvedValue(mockedResolvedData as any);
   });
 
   it('should return error correctly', async () => {
     // Arrange
-    const { result } = renderHook(() => useInputAmount(mockedForm));
+    mockedAddBillItem.mockResolvedValue(mockedResolvedError as any);
+    const { result } = renderHook(({ mockedForm }) => useInputAmount(mockedForm), { initialProps: { mockedForm } });
+    // Act
+    await act(() => {
+      result.current.onFinish({ amount: 100, date: '2021-01-01', category: 'income', tags: ['Food'] });
+    });
+    // Assert
+    await waitFor(() => {
+      expect(mockedAddBillItem).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  it('should return data correctly', async () => {
+    // Arrange
+    mockedAddBillItem.mockResolvedValue(mockedResolvedData as any);
+    const { result } = renderHook(({ mockedForm }) => useInputAmount(mockedForm), { initialProps: { mockedForm } });
     // Act
     await act(() => {
       result.current.onFinish({ amount: 100, date: '2021-01-01', category: 'income', tags: ['Food'] });
