681c7a7bee2f47f6752fa780d4763c32da3125e2
YG1022 <958065532@qq.com>
2023-03-20 10:15:19 +0800

    chore: remove unused test for AccountBook

--- src/pages/AccountBook/AccountBook.test.tsx
+++ src/pages/AccountBook/AccountBook.test.tsx
@@ -43,43 +43,10 @@ describe('AccountBook', () => {
 
   it('should deliver specific props', () => {
     // Arrange
-    const { container } = render(<AccountBook />);
+    render(<AccountBook />);
     // Act
 
     // Assert
     expect(screen.getByText('Income List')).toBeInTheDocument();
   });
-
-  //   it('should render text input on screen', async () => {
-  //     // Arrange
-  //     // Act
-  //     // Assert
-  //     render(<AccountBook />);
-  //     await userEvent.type(screen.getByTestId('input-bar'), '11');
-  //     await userEvent.click(screen.getByRole('button', { name: 'Assign' }));
-  //     const amount = screen.getByTestId('amount').innerHTML;
-  //     await waitFor(() => {
-  //       expect(amount).toBe('11');
-  //     });
-  //   });
-
-  //   it('should render 0 if input character not number', async () => {
-  //     render(<AccountBook />);
-  //     await userEvent.type(screen.getByTestId('input-bar'), 'character');
-  //     await userEvent.click(screen.getByRole('button', { name: 'Assign' }));
-  //     const amount = screen.getByTestId('amount').innerHTML;
-  //     await waitFor(() => {
-  //       expect(amount).toBe('0');
-  //     });
-  //   });
-
-  //   it('should render negative number', async () => {
-  //     render(<AccountBook />);
-  //     await userEvent.type(screen.getByTestId('input-bar'), '-13');
-  //     await userEvent.click(screen.getByRole('button', { name: 'Assign' }));
-  //     const amount = screen.getByTestId('amount').innerHTML;
-  //     await waitFor(() => {
-  //       expect(amount).toBe('-13');
-  //     });
-  //   });
 });
