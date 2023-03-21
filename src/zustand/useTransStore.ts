17938b7dc49e3afb9ad09584dfdb3f1811a71f3b
YG1022 <958065532@qq.com>
2023-03-21 11:33:46 +0800

    feat: persist to session storage

--- src/zustand/useTransStore.ts
+++ src/zustand/useTransStore.ts
@@ -1,15 +1,23 @@
-import create from 'zustand';
+import { create } from 'zustand';
+import { persist, createJSONStorage } from 'zustand/middleware';
 import produce from 'immer';
 import { transItem } from '../constants/types';
 
-interface transItemState {
-  amountList: transItem[];
+interface transState {
+  amountList: Array<transItem>;
   setTrans: (data: Array<transItem>) => void;
 }
 
-export const useTransStore = create<transItemState>((set, get) => ({
-  amountList: [],
-  setTrans: (data: Array<transItem>) => {
-    set({ amountList: produce(get().amountList, () => data)});
-  },
-}));
+export const useTransStore = create<transState>()(
+  persist(
+    (set, get) => ({
+      amountList: [],
+      setTrans: (data: Array<transItem>) =>
+        set({ amountList: produce(get().amountList, () => data) }),
+    }),
+    {
+      name: 'trans-storage',
+      storage: createJSONStorage(() => sessionStorage),
+    }
+  )
+);
