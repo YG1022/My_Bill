7d8c29de090f25eb14cfad1351d2d18c144179e0
YG1022 <958065532@qq.com>
2023-03-21 14:51:27 +0800

    feat: init a button for delete

--- src/components/TransList/TransList.tsx
+++ src/components/TransList/TransList.tsx
@@ -1,4 +1,4 @@
-import { Divider, List, Tag } from 'antd';
+import { Button, Divider, List, Modal, Tag } from 'antd';
 import React from 'react';
 import { transListProps } from '../../constants/types';
 import { useTransList } from './hooks/useTransList';
@@ -7,29 +7,50 @@ import './TransList.scss';
 const TransList: React.FC<transListProps> = ({ amountList, category }) => {
   const { transactions, totalAmount } = useTransList(amountList, category);
 
+  const deleteTrans = (id: number) => {
+    const deleteSelectedTrans = () => {
+      console.log('delete');
+    };
+
+    return () => {
+      Modal.confirm({
+        title: `Are you really want to destroy this transaction？`,
+        okText: 'Sure',
+        cancelText: 'Cancel',
+        centered: true,
+        onOk: deleteSelectedTrans,
+      });
+    };
+  };
+
   return (
-    <div className="bills-list">
-      <Divider className="bills-divider" orientation="left">
+    <div className='bills-list'>
+      <Divider className='bills-divider' orientation='left'>
         {category === '+' ? 'Income' : 'Expenses'} List
       </Divider>
-      <div className="amount-content">
+      <div className='amount-content'>
         <span>The amount of all the {category === '+' ? 'income' : 'expenses'} is $</span>
-        <span className="amount" data-testid="amount">
+        <span className='amount' data-testid='amount'>
           {totalAmount}
         </span>
       </div>
       <List
-        className="list-content"
-        data-testid="list-content"
+        className='list-content'
+        data-testid='list-content'
         bordered
         dataSource={transactions}
         renderItem={transaction => (
-          <List.Item data-testid="item">
-            <span className="item-amount">{transaction.amount}</span>
-            <span className="item-tag">
+          <List.Item data-testid='item'>
+            <span className='item-amount'>{transaction.amount}</span>
+            <span className='item-tag'>
               {transaction.tags.map((tag, index) => (
                 <Tag key={index}>{tag}</Tag>
               ))}
+            </span>
+            <span>
+              <Button type='link' className='item-delete' onClick={deleteTrans(transaction.id)}>
+                Delete
+              </Button>
             </span>
           </List.Item>
         )}
