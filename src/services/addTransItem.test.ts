2f17ae399ebf3dc2e59559165618591855e40ef3
YG1022 <958065532@qq.com>
2023-03-22 10:34:18 +0800

    fix: add new input props

--- src/services/addTransItem.test.ts
+++ src/services/addTransItem.test.ts
@@ -2,10 +2,10 @@ import { waitFor } from '@testing-library/react';
 import { supabaseClient } from '../supabaseClient';
 import { addTransItem } from './addTransItem';
 
-const mockResultData = [{
-  data: [{ id: 1, date: '2023-03-17', amount: 100, category: '+', tags: ['tag 1'] }],
+const mockResultData = {
+  data: [{ id: 1, date: '2023-03-17', amount: '100', category: '+', tags: ['tag 1'] }],
   error: null,
-}];
+};
 
 jest.mock('../supabaseClient');
 const mockedSupabaseClient = supabaseClient as jest.Mocked<typeof supabaseClient>;
@@ -28,7 +28,7 @@ describe('add transaction items', () => {
 
     // Assert
     await waitFor(async () => {
-      return expect(await addTransItem({ tags: '' })).toEqual(mockResultData);
+      return expect(await addTransItem({ amount: '100', category: '+', tags: [] }, null)).toEqual(mockResultData);
     });
   });
 });
