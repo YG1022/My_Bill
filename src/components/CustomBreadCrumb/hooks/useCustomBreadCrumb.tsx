cb2031fc1e3797715133d54db827f1a577a10c48
YG1022 <958065532@qq.com>
2023-03-22 13:42:35 +0800

    feat: should filter pathname without number

--- src/components/CustomBreadCrumb/hooks/useCustomBreadCrumb.tsx
+++ src/components/CustomBreadCrumb/hooks/useCustomBreadCrumb.tsx
@@ -4,8 +4,12 @@ import { breadcrumbNameMap } from '../../../constants/routes';
 
 const useCustomBreadCrumb = () => {
   const location = useLocation();
+  const numberReg = /^\d+$/;
 
-  const pathSnippets = location.pathname.split('/').filter(i => i);
+  const pathSnippets = location.pathname
+    .split('/')
+    .filter(i => !numberReg.test(i))
+    .filter(i => i);
   const extraBreadcrumbItems = pathSnippets.map((_, index) => {
     const url = `/${pathSnippets.slice(0, index + 1).join('/')}`;
     return {
