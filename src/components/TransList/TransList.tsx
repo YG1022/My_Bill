84660ea3e5d5eea3d5c7dfb03512211977d9340f
YG1022 <958065532@qq.com>
2023-03-23 23:28:48 +0800

    fix: fix automatically checked box

--- src/components/TransList/TransList.tsx
+++ src/components/TransList/TransList.tsx
@@ -1,53 +1,61 @@
 import { Button, Checkbox, Divider, List, Tag } from 'antd';
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { transListProps } from '../../constants/types';
 import { useTransList } from './hooks/useTransList';
 import './TransList.scss';
 import { NavLink } from 'react-router-dom';
-import { CheckboxChangeEvent } from 'antd/es/checkbox';
 import { useTransStore } from '../../stores/useTransStore';
 import { shallow } from 'zustand/shallow';
 
 const TransList: React.FC<transListProps> = ({ amountList, category }) => {
   const { transactions, totalAmount, deleteTrans } = useTransList(amountList, category);
-  const { selectedId, setSelectedId } = useTransStore(
+  const { selectedId, setSelectedId, clearSelectedId } = useTransStore(
     state => ({
       selectedId: state.selectedId,
       setSelectedId: state.setSelectedId,
+      clearSelectedId: state.clearSelectedId,
     }),
-    shallow
+    shallow,
   );
+  const { current: idsDeps } = useRef(selectedId);
+
+  useEffect(() => {
+    clearSelectedId();
+  }, [idsDeps]);
 
   return (
-    <div className="bills-list">
-      <Divider className="bills-divider" orientation="left">
+    <div className='bills-list'>
+      <Divider className='bills-divider' orientation='left'>
         {category === '+' ? 'Income' : 'Expenses'} List
       </Divider>
-      <div className="amount-content">
+      <div className='amount-content'>
         <span>The amount of all the {category === '+' ? 'income' : 'expenses'} is $</span>
-        <span className="amount" data-testid="amount">
+        <span className='amount' data-testid='amount'>
           {totalAmount.toString()}
         </span>
-        <Button type="link" className="delete-selected" onClick={deleteTrans(selectedId)}>
+        <Button type='link' className='delete-selected' onClick={deleteTrans(selectedId)}>
           Delete
         </Button>
       </div>
       <List
-        className="list-content"
-        data-testid="list-content"
+        className='list-content'
+        data-testid='list-content'
         bordered
         dataSource={transactions}
         renderItem={transaction => (
-          <List.Item data-testid="item">
-            <Checkbox onChange={(e: CheckboxChangeEvent) => setSelectedId(transaction.id)} />
-            <span className="item-amount">{transaction.amount}</span>
-            <span className="item-tag">
+          <List.Item data-testid='item'>
+            <Checkbox
+              checked={selectedId.includes(transaction.id)}
+              onChange={() => setSelectedId(transaction.id)}
+            />
+            <span className='item-amount'>{transaction.amount}</span>
+            <span className='item-tag'>
               {transaction.tags.map((tag, index) => (
                 <Tag key={index}>{tag}</Tag>
               ))}
             </span>
             <NavLink to={`/transactions/trans-edit/${transaction.id}`}>Edit</NavLink>
-            <Button type="link" className="item-delete" onClick={deleteTrans(transaction.id)}>
+            <Button type='link' className='item-delete' onClick={deleteTrans(transaction.id)}>
               Delete
             </Button>
           </List.Item>
