0f36b0c71b8c4ea8d1b7258f39703590f5de0695
YG1022 <958065532@qq.com>
2023-03-27 17:05:20 +0800

    feat: init profile type

--- src/constants/types.ts
+++ src/constants/types.ts
@@ -26,4 +26,12 @@ type user = {
   confirmpassword: string;
 }
 
-export { transItem, transListProps, MenuItem, Props, user };
+type profile = {
+  email: string;
+  prefix: string;
+  phonenumber: string;
+  introduction: string;
+  gender: string;
+}
+
+export { transItem, transListProps, MenuItem, Props, user, profile };
