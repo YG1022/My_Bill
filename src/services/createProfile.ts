0d414c5ee266430d171fb83e51f94fcfc5889c51
YG1022 <958065532@qq.com>
2023-03-28 17:23:55 +0800

    feat: justify attributes of data post to db

--- src/services/createProfile.ts
+++ src/services/createProfile.ts
@@ -2,14 +2,20 @@ import { supabaseClient } from '../supabaseClient';
 import { profile } from '../constants/types';
 
 const createProfile = (profile: profile) => {
+  const { email, realname, gender, prefix, phonenumber, birthday } = profile;
+
   return supabaseClient
     .from('profiles')
-    .insert([{
-      email: profile.email,
-      phone_number: profile.phonenumber,
-      introduction: profile.introduction,
-      gender: profile.gender,
-    }])
+    .insert([
+      {
+        email: email,
+        real_name: realname,
+        gender: gender,
+        prefix: prefix,
+        phone_number: phonenumber,
+        birthday: birthday,
+      },
+    ])
     .select();
 };
 
