0336ef06f93a7b64db624726c65cbaa06aea882b
YG1022 <958065532@qq.com>
2023-03-29 14:53:45 +0800

    feat: repeatability check for e-mail

--- src/pages/Registration/EditProfiles.tsx
+++ src/pages/Registration/EditProfiles.tsx
@@ -4,16 +4,17 @@ import { PageContainer } from '../../components/PageContainer/PageContainer';
 import { NavLink, useNavigate } from 'react-router-dom';
 import createProfile from '../../services/createProfile';
 import { ROUTES } from '../../constants/routes';
+import { supabaseClient } from '../../supabaseClient';
 
 const EditProfiles = () => {
   const navigateTo = useNavigate();
   const [form] = Form.useForm();
 
   const prefixSelector = (
-    <Form.Item name="prefix" noStyle>
+    <Form.Item name='prefix' noStyle>
       <Select>
-        <Select.Option value="86">+86</Select.Option>
-        <Select.Option value="852">+852</Select.Option>
+        <Select.Option value='86'>+86</Select.Option>
+        <Select.Option value='852'>+852</Select.Option>
       </Select>
     </Form.Item>
   );
@@ -30,11 +31,20 @@ const EditProfiles = () => {
   const dateFormat = 'YYYY/MM/DD';
 
   const finilizeProfiles = async () => {
-    const { birthday, ...extraData } = form.getFieldsValue();
-    const postData = { birthday: birthday?.format('YYYY/MM/DD'), ...extraData };
+    const { email, birthday, ...extraData } = form.getFieldsValue();
+    const { data, error } = await supabaseClient.from('profiles').select().eq('email', email);
+    if (data.length > 0) {
+      alert('This email has been used. Please choose another email.');
+      return;
+    } else if (error) {
+      alert('Something wrong happened. Please try again later.');
+      return;
+    } else {
+      const postData = { birthday: birthday?.format('YYYY/MM/DD'), email, ...extraData };
 
-    await createProfile(postData);
-    navigateTo(ROUTES.home);
+      await createProfile(postData);
+      navigateTo(ROUTES.home);
+    }
   };
 
   return (
@@ -43,7 +53,7 @@ const EditProfiles = () => {
         <Form
           {...formItemLayout}
           form={form}
-          name="editprofiles"
+          name='editprofiles'
           onFinish={finilizeProfiles}
           initialValues={{ prefix: '86' }}
           style={{
@@ -55,8 +65,8 @@ const EditProfiles = () => {
           scrollToFirstError
         >
           <Form.Item
-            label="E-mail"
-            name="email"
+            label='E-mail'
+            name='email'
             rules={[
               {
                 type: 'email',
@@ -68,43 +78,43 @@ const EditProfiles = () => {
               },
             ]}
           >
-            <Input placeholder="Please input your E-mail!" />
+            <Input placeholder='Please input your E-mail!' />
           </Form.Item>
-          <Form.Item label="Real Name" name="realname" tooltip="Your name in the real world!">
-            <Input placeholder="Please input your real name!" />
+          <Form.Item label='Real Name' name='realname' tooltip='Your name in the real world!'>
+            <Input placeholder='Please input your real name!' />
           </Form.Item>
-          <Form.Item label="Gender" name="gender">
-            <Select placeholder="Please select your gender!">
-              <Select.Option value="male">Male</Select.Option>
-              <Select.Option value="female">Female</Select.Option>
-              <Select.Option value="other">Other</Select.Option>
+          <Form.Item label='Gender' name='gender'>
+            <Select placeholder='Please select your gender!'>
+              <Select.Option value='male'>Male</Select.Option>
+              <Select.Option value='female'>Female</Select.Option>
+              <Select.Option value='other'>Other</Select.Option>
             </Select>
           </Form.Item>
-          <Form.Item label="Phone Number" name="phonenumber">
+          <Form.Item label='Phone Number' name='phonenumber'>
             <Input
               addonBefore={prefixSelector}
               style={{ width: '100%' }}
-              placeholder="Please input your phone number!"
+              placeholder='Please input your phone number!'
             />
           </Form.Item>
-          <Form.Item label="Birthday" name="birthday" rules={[{ type: 'date' }]}>
+          <Form.Item label='Birthday' name='birthday' rules={[{ type: 'date' }]}>
             <DatePicker
               style={{ width: 333.33 }}
               format={dateFormat}
-              placeholder="Select your birthday!"
+              placeholder='Select your birthday!'
             />
           </Form.Item>
           <Form.Item {...tailFormItemLayout}>
             <Button
               style={{ marginRight: 30 }}
-              type="primary"
+              type='primary'
               onClick={async () => {
                 await createProfile(form.getFieldsValue());
               }}
             >
               <NavLink to={ROUTES.home}>Skip</NavLink>
             </Button>
-            <Button type="primary" htmlType="submit">
+            <Button type='primary' htmlType='submit'>
               Submit
             </Button>
           </Form.Item>
