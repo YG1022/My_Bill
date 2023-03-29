d02830ef0289b707cb95ce17bc05ca4b02aafe67
YG1022 <958065532@qq.com>
2023-03-29 10:12:46 +0800

    feat: import sign in page to router

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import InputAmount from './pages/InputAmount/InputAmount';
 import { ROUTES } from './constants/routes';
 import Registration from './pages/Registration/Registration';
 import EditProfiles from './pages/Registration/EditProfiles';
+import Signin from './pages/Signin/Signin';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -17,6 +18,7 @@ root.render(
   <React.StrictMode>
     <Router>
       <Routes>
+        <Route path={ROUTES.signIn} element={<Signin />} />
         <Route path={ROUTES.registration} element={<Registration />} />
         <Route path={ROUTES.profilesEdit} element={<EditProfiles />} />
         <Route path={ROUTES.home} element={<App />}>
