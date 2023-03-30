2e2c7e8926911d31f7c354e1a5991ed35242a6b6
YG1022 <958065532@qq.com>
2023-03-30 18:06:06 +0800

    test: test for register derivative utils

---
+++ src/pages/Registration/utils/RegisterDerivativeUtils.test.tsx
@@ -0,0 +1,7 @@
+import RegisterDerivativeUtils from "./RegisterDerivativeUtils";
+
+describe("RegisterDerivativeUtils", () => {
+  it("should be defined", () => {
+    expect(RegisterDerivativeUtils).toBeDefined();
+  });
+});
