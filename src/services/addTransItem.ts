91ac58480cd0c8ab4e531f3e30acbf1a41a8a041
YG1022 <958065532@qq.com>
2023-03-30 16:36:03 +0800

    feat: should insert the uuid for current user to db when create transaction

--- src/services/addTransItem.ts
+++ src/services/addTransItem.ts
@@ -1,23 +1,24 @@
-import { supabaseClient } from '../supabaseClient';
-import { transItem } from '../constants/types';
-import { PostgrestError } from '@supabase/supabase-js';
+import { supabaseClient } from "../supabaseClient";
+import { transItem } from "../constants/types";
+import { PostgrestError } from "@supabase/supabase-js";
 
 const addTransItem = async (values, id: string | null): Promise<{ data: transItem[], error: PostgrestError }> => {
   const { tags, ...extraData } = values;
   const formData = { tags: tags.tags, ...extraData };
+  const uuid = localStorage.getItem("uuid");
 
   if (id === null) {
     const { data, error } = await supabaseClient
-      .from('transactions')
-      .insert([{ amount: formData.amount, category: formData.category, tags: formData.tags }])
+      .from("transactions")
+      .insert([{ amount: formData.amount, category: formData.category, tags: formData.tags, uuid }])
       .select<any, transItem>();
     return { data, error };
   }
   if (id) {
     const { data, error } = await supabaseClient
-      .from('transactions')
-      .update({ amount: formData.amount, category: formData.category, tags: formData.tags })
-      .eq('id', Number(id))
+      .from("transactions")
+      .update({ amount: formData.amount, category: formData.category, tags: formData.tags, uuid })
+      .eq("id", Number(id))
       .select<any, transItem>();
     return { data, error };
   }
