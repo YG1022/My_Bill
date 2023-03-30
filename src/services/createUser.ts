5b765d44e21c776317ce5cd2ca64f1e589d8f1db
YG1022 <958065532@qq.com>
2023-03-30 11:19:06 +0800

    feat: insert uuid to db when register

--- src/services/createUser.ts
+++ src/services/createUser.ts
@@ -1,12 +1,13 @@
-import { user } from '../constants/types';
-import { supabaseClient } from '../supabaseClient';
+import { user } from "../constants/types";
+import { supabaseClient } from "../supabaseClient";
+import uuid from "react-uuid";
 
 const createUser = (user: user) => {
   const { accountname, password } = user;
 
   return supabaseClient
-    .from('users')
-    .insert([{ account_name: accountname, password: password }])
+    .from("users")
+    .insert([{ account_name: accountname, password: password, uuid: uuid() }])
     .select();
 };
 
