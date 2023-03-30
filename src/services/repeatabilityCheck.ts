84e915f7002dea95605c65d7dc1d5b4badc196f3
YG1022 <958065532@qq.com>
2023-03-30 11:26:24 +0800

    feat: add type for select from db

--- src/services/repeatabilityCheck.ts
+++ src/services/repeatabilityCheck.ts
@@ -1,21 +1,18 @@
-import { supabaseClient } from '../supabaseClient';
-import { fetchUser } from '../constants/types';
+import { supabaseClient } from "../supabaseClient";
+import { fetchProfile, fetchUser } from "../constants/types";
 
 const repeatabilityCheck = (accountname, email, phonenumber) => {
   const nameCheck = supabaseClient
-    .from('users')
+    .from("users")
     .select<any, fetchUser>()
-    .eq('account_name', accountname);
+    .eq("account_name", accountname);
 
-  const emailCheck = supabaseClient
-    .from('profiles')
-    .select()
-    .eq('email', email);
+  const emailCheck = supabaseClient.from("profiles").select<any, fetchProfile>().eq("email", email);
 
   const phoneCheck = supabaseClient
-    .from('profiles')
-    .select()
-    .eq('phone_number', phonenumber);
+    .from("profiles")
+    .select<any, fetchProfile>()
+    .eq("phone_number", phonenumber);
 
   return { nameCheck, emailCheck, phoneCheck };
 };
