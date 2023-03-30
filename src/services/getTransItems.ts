4135ed629152a40a791f9b3473a93471d28e27d7
YG1022 <958065532@qq.com>
2023-03-30 16:39:27 +0800

    feat: should check the uuid when fetch transactions from db

--- src/services/getTransItems.ts
+++ src/services/getTransItems.ts
@@ -1,16 +1,22 @@
-import { supabaseClient } from '../supabaseClient';
-import { transItem } from '../constants/types';
-import { PostgrestError } from '@supabase/supabase-js';
+import { supabaseClient } from "../supabaseClient";
+import { transItem } from "../constants/types";
+import { PostgrestError } from "@supabase/supabase-js";
 
 const getTransItems = async (id?: number): Promise<{ data: transItem[]; error: PostgrestError }> => {
+  const uuid = localStorage.getItem("uuid");
+
   if (id) {
     const { data, error } = await supabaseClient
-      .from('transactions')
+      .from("transactions")
       .select<any, transItem>()
-      .eq('id', id);
+      .eq("uuid", uuid)
+      .eq("id", id);
     return { data, error };
   } else {
-    const { data, error } = await supabaseClient.from('transactions').select<any, transItem>();
+    const { data, error } = await supabaseClient
+      .from("transactions")
+      .select<any, transItem>()
+      .eq("uuid", uuid);
     return { data, error };
   }
 };
