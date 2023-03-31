d97d95848635ab307af1ad6a1813ae7ca93ada73
YG1022 <958065532@qq.com>
2023-03-31 11:03:31 +0800

    fix: make create user func a Promise

--- src/services/createUser.ts
+++ src/services/createUser.ts
@@ -1,14 +1,17 @@
-import { user } from "../constants/types";
+import { fetchUser, user } from "../constants/types";
 import { supabaseClient } from "../supabaseClient";
 import uuid from "react-uuid";
+import { PostgrestError } from "@supabase/supabase-js";
 
-const createUser = (user: user) => {
+const createUser = async (user: user): Promise<{ data: fetchUser[], error: PostgrestError }> => {
   const { accountname, password } = user;
 
-  return supabaseClient
+  const { data, error } = await supabaseClient
     .from("users")
     .insert([{ account_name: accountname, password: password, uuid: uuid() }])
-    .select();
+    .select<any, fetchUser>();
+
+  return { data, error };
 };
 
 export default createUser;
