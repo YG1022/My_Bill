58c8f4179a8130e56c59fd62e514b219215ef7a8
YG1022 <958065532@qq.com>
2023-03-31 09:37:11 +0800

    test: add a test for register hook temporarily

---
+++ src/pages/Registration/hooks/useRegistration.test.ts
@@ -0,0 +1,47 @@
+import useRegistration from "./useRegistration";
+import { renderHook, waitFor } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import repeatabilityCheck from "../../../services/repeatabilityCheck";
+
+jest.mock("../../../services/repeatabilityCheck");
+const mockedRepeatabilityCheck = repeatabilityCheck as jest.MockedFunction<
+  typeof repeatabilityCheck
+>;
+
+describe("useRegistration", () => {
+  const mockedForm = {
+    getFieldsValue: jest.fn(),
+  };
+  const mockedNameCheck = jest.fn().mockReturnValue({
+    data: [],
+    error: null,
+    status: 200,
+    statusText: "",
+  });
+  mockedRepeatabilityCheck.mockReturnValue({
+    nameCheck: mockedNameCheck,
+    emailCheck: jest.fn(),
+    phoneCheck: jest.fn(),
+  });
+
+  beforeEach(() => {
+    mockedForm.getFieldsValue.mockClear();
+    mockedNameCheck.mockClear();
+  });
+
+  it("should call with Happy Path", async () => {
+    // Arrange
+    const { result } = renderHook(() => useRegistration(mockedForm), {
+      wrapper: MemoryRouter,
+    });
+    // Act
+    const { toNextStep } = result.current;
+    // await result.current.toNextStep();
+    // Assert
+    await waitFor(() => {
+      expect(typeof toNextStep).toBe("function");
+      //   expect(mockedForm.getFieldsValue).toHaveBeenCalledTimes(1);
+      //   expect(mockedNameCheck).toHaveBeenCalledTimes(1);
+    });
+  });
+});
