9f516ecd68febd5c937f8cb836b8aae8b67c3226
YG1022 <958065532@qq.com>
2023-03-31 15:54:13 +0800

    feat: should search uuid from 'users' first

--- src/services/getProfile.ts
+++ src/services/getProfile.ts
@@ -1,14 +1,23 @@
-import { fetchProfile } from "../constants/types";
+import { fetchProfile, fetchUser } from "../constants/types";
 import { PostgrestError } from "@supabase/supabase-js";
 import { supabaseClient } from "../supabaseClient";
 
 const getProfile = async (
   uuid: string
 ): Promise<{ data: fetchProfile[]; error: PostgrestError }> => {
+  const { data: temData } = await supabaseClient
+    .from("users")
+    .select<any, fetchUser>()
+    .eq("uuid", uuid);
+
+  const id = temData[0].id;
+
   const { data, error } = await supabaseClient
     .from("profiles")
     .select<any, fetchProfile>()
-    .eq("uuid", uuid);
+    .eq("id", id);
 
   return { data, error };
 };
+
+export default getProfile;
