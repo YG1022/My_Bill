57f73967ba66877b7e42680ff901165f18ae1f78
YG1022 <958065532@qq.com>
2023-03-31 10:20:32 +0800

    fix: add eq attribute for get func

--- src/services/getTransItems.test.ts
+++ src/services/getTransItems.test.ts
@@ -1,26 +1,22 @@
-import { waitFor } from '@testing-library/react';
-import { supabaseClient } from '../supabaseClient';
-import { getTransItems } from './getTransItems';
+import { waitFor } from "@testing-library/react";
+import { supabaseClient } from "../supabaseClient";
+import { getTransItems } from "./getTransItems";
 
 const mockResultData = {
-  data: [{ id: 1, date: '2023-03-17', amount: '100', category: '+', tags: ['tag 1'] }],
+  data: [{ id: 1, date: "2023-03-17", amount: "100", category: "+", tags: ["tag 1"] }],
   error: null,
 };
-jest.mock('../supabaseClient');
+jest.mock("../supabaseClient");
 const mockedSupabaseClient = supabaseClient as jest.Mocked<typeof supabaseClient>;
 
-describe('getTransItems from supabase', () => {
+describe("getTransItems from supabase", () => {
   const fromMock = mockedSupabaseClient.from as any;
 
-  beforeEach(() => {
-    fromMock.mockReturnValue({
-      select: jest.fn().mockReturnValue(mockResultData),
-    });
-  });
-
-  it('should get all trans items', async () => {
+  it("should get all trans items", async () => {
     // Arrange
-
+    fromMock.mockReturnValue({
+      select: jest.fn().mockReturnValue({ eq: jest.fn().mockReturnValue(mockResultData) }),
+    });
     // Act
 
     // Assert
