7eab9bd3fb9d6a54365496aa8ca7b7515f76b4d0
YG1022 <958065532@qq.com>
2023-04-03 15:49:51 +0800

    feat: button link to account page

--- src/components/Sider/MenuSider.tsx
+++ src/components/Sider/MenuSider.tsx
@@ -1,17 +1,40 @@
-import { Layout, Menu } from 'antd';
-import React from 'react';
-import { useMenuSider } from './hooks/useMenuSider';
+import { Button, Layout, Menu, Popover, Tooltip } from 'antd'
+import React, { useEffect, useState } from 'react'
+import { useMenuSider } from './hooks/useMenuSider'
+import { useAccountStore } from '../../stores/useAccountStore'
+import { NavLink } from 'react-router-dom'
+import { ROUTES } from '../../constants/routes'
 
 const MenuSider: React.FC = () => {
-  const { Sider } = Layout;
-  const { collapsed, setCollapsed, menus } = useMenuSider();
+  const { Sider } = Layout
+  const { collapsed, setCollapsed, menus } = useMenuSider()
+  const { user } = useAccountStore(state => ({ user: state.user }))
+  const [open, setOpen] = useState(false)
+
+  const hide = () => setOpen(false)
+  const handleOpenChange = (newOpen: boolean) => setOpen(newOpen)
 
   return (
     <Sider collapsible collapsed={collapsed} onCollapse={value => setCollapsed(value)}>
-      <div className="search-holder" />
+      <Popover
+        className="search-holder"
+        content={
+          <NavLink to={ROUTES.profiles} onClick={hide}>
+            Profile
+          </NavLink>
+        }
+        title="Account"
+        trigger="click"
+        open={open}
+        onOpenChange={handleOpenChange}
+      >
+        <Tooltip title="Account">
+          <Button type="primary">{user.account_name}</Button>
+        </Tooltip>
+      </Popover>
       <Menu theme="dark" mode="inline" items={menus} />
     </Sider>
-  );
-};
+  )
+}
 
-export default MenuSider;
+export default MenuSider
