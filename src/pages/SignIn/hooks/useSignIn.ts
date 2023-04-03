39928e90d772315b6f2640a1c214ee335e3e580a
YG1022 <958065532@qq.com>
2023-04-03 23:24:37 +0800

    fix: should remove uuid after validation failure

--- src/pages/SignIn/hooks/useSignIn.ts
+++ src/pages/SignIn/hooks/useSignIn.ts
@@ -9,26 +9,19 @@ const useSignIn = form => {
 
   const signIn = async () => {
     const { flag, checkData } = await userSignIn(form)
-
     localStorage.setItem('signedIn', flag.toString())
 
     if (!flag) {
-      form.setFields([
-        {
-          name: 'password',
-          errors: ['Wrong user name or password, please try again!'],
-        },
-      ])
-      return
+      form.setFields([{
+        name: 'password',
+        errors: ['Wrong user name or password, please try again!'],
+      }])
+      localStorage.removeItem('uuid')
+    } else {
+      setUser(checkData)
+      localStorage.setItem('uuid', checkData.uuid)
+      navigateTo(ROUTES.home)
     }
-
-    setTimeout(() => {
-      if (flag) {
-        setUser(checkData)
-        localStorage.setItem('uuid', checkData.uuid)
-        navigateTo(ROUTES.home)
-      }
-    }, 1000)
   }
 
   return { signIn }
