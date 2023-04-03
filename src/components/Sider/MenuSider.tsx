88ad827857c3c0f0a4a5379506e1924d5fe99aaf
YG1022 <958065532@qq.com>
2023-04-03 23:23:54 +0800

    feat: add sign out link

--- src/components/Sider/MenuSider.tsx
+++ src/components/Sider/MenuSider.tsx
@@ -1,20 +1,32 @@
 import { Button, Layout, Menu, Popover, Tooltip } from 'antd'
+import { LogoutOutlined } from '@ant-design/icons'
 import React from 'react'
 import { useMenuSider } from './hooks/useMenuSider'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
 import { ROUTES } from '../../constants/routes'
 
 const MenuSider: React.FC = () => {
   const { Sider } = Layout
   const { collapsed, setCollapsed, open, setOpen, menus, name, sliceLen } = useMenuSider()
+  const navigateTo = useNavigate()
+
+  const signOut = (event) => {
+    event.preventDefault()
+    localStorage.removeItem('signedIn')
+    localStorage.removeItem('uuid')
+    navigateTo(ROUTES.signIn)
+  }
 
   return (
-    <Sider collapsible collapsed={collapsed} onCollapse={value => setCollapsed(value)}>
+    <Sider className='side-bar' collapsible collapsed={collapsed} onCollapse={value => setCollapsed(value)}>
       <Popover
-        className='search-holder'
-        content={
-          <NavLink to={ROUTES.profiles} onClick={() => setOpen(false)}>Profile</NavLink>
-        }
+        className='account-holder'
+        content={(
+          <div className='account-content'>
+            <NavLink style={{ display: 'block' }} to={ROUTES.profiles} onClick={() => setOpen(false)}>Profile</NavLink>
+            <NavLink to={ROUTES.signIn} onClick={signOut}>Sign Out</NavLink>
+          </div>
+        )}
         trigger='click'
         open={open}
         onOpenChange={(newOpen: boolean) => setOpen(newOpen)}
