f18656bb49c0a145f5da6ba9be617b83423263ae
YG1022 <958065532@qq.com>
2023-04-04 12:27:33 +0800

    refactor: use useRoutes replace traditional Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,66 @@
-import './App.scss';
-import { ConfigProvider, Layout } from 'antd';
-import React, { useEffect, useRef } from 'react';
-import { Outlet } from 'react-router-dom';
-import MenuSider from './components/Sider/MenuSider';
-import CustomBreadCrumb from './components/CustomBreadCrumb/CustomBreadCrumb';
-import { useAccountBook } from './pages/AccountBook/hooks/useAccountBook';
-
-const { Header, Footer, Content } = Layout;
+import './App.scss'
+import React, { useEffect, useRef } from 'react'
+import { useAccountBook } from './pages/AccountBook/hooks/useAccountBook'
+import { useRoutes } from 'react-router-dom'
+import { ROUTES } from './constants/routes'
+import SignIn from './pages/SignIn/SignIn'
+import Registration from './pages/Registration/Registration'
+import EditProfiles from './pages/Registration/EditProfiles'
+import AppContent from './components/AppContent'
+import HomePage from './pages/Home/HomePage'
+import { PrivateRoute } from './utils/PrivateRoute'
+import InputAmount from './pages/InputAmount/InputAmount'
+import AccountBook from './pages/AccountBook/AccountBook'
 
 const App: React.FC = () => {
-  const themeConfig = { token: { colorPrimary: '#faad14' } };
-  const { amountList, fetchData } = useAccountBook();
-  const { current: transDeps } = useRef(amountList);
+  const { amountList, fetchData } = useAccountBook()
+  const { current: transDeps } = useRef(amountList)
 
   useEffect(() => {
-    fetchData();
-  }, [transDeps]);
+    fetchData()
+  }, [transDeps])
 
-  return (
-    <Layout className="app">
-      <ConfigProvider theme={themeConfig}>
-        <MenuSider />
-        <Layout>
-          <Header className="header">My Bill</Header>
-          <Content className="layout-content">
-            <CustomBreadCrumb />
-            <div className="main">
-              <Outlet />
-            </div>
-          </Content>
-          <Footer className="site-footer">Saving is a virtue.</Footer>
-        </Layout>
-      </ConfigProvider>
-    </Layout>
-  );
-};
+  const routes = useRoutes([
+    {
+      path: ROUTES.signIn,
+      element: <SignIn />,
+    },
+    {
+      path: ROUTES.registration,
+      element: <Registration />,
+    },
+    {
+      path: ROUTES.profilesEdit,
+      element: <EditProfiles />,
+    },
+    {
+      path: ROUTES.home,
+      element: <PrivateRoute component={AppContent} />,
+      children: [
+        {
+          element: <PrivateRoute component={HomePage} />,
+        },
+        {
+          path: ROUTES.transactionInput,
+          element: <PrivateRoute component={InputAmount} />,
+        },
+        {
+          path: ROUTES.transactions,
+          element: <PrivateRoute component={AccountBook} />,
+        },
+        {
+          path: ROUTES.transactionEdit,
+          element: <PrivateRoute component={InputAmount} />,
+        },
+        {
+          path: ROUTES.profiles,
+          element: <PrivateRoute component={EditProfiles} />,
+        },
+      ],
+    },
+  ])
 
-export default App;
+  return routes
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,35 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import "./styles/index.scss";
-import App from "./App";
-import reportWebVitals from "./utils/reportWebVitals";
-import HomePage from "./pages/Home/HomePage";
-import AccountBook from "./pages/AccountBook/AccountBook";
-import InputAmount from "./pages/InputAmount/InputAmount";
-import { ROUTES } from "./constants/routes";
-import Registration from "./pages/Registration/Registration";
-import EditProfiles from "./pages/Registration/EditProfiles";
-import SignIn from "./pages/SignIn/SignIn";
-import { authRoute } from "./utils/authRoute";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router } from 'react-router-dom'
+import './styles/index.scss'
+import App from './App'
+import reportWebVitals from './utils/reportWebVitals'
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
   <Router>
-    <Routes>
-      <Route path={ROUTES.signIn} element={<SignIn />} />
-      <Route path={ROUTES.registration} element={<Registration />} />
-      <Route path={ROUTES.profilesEdit} element={<EditProfiles />} />
-      <Route path={ROUTES.home} element={authRoute(<App />)}>
-        <Route index element={authRoute(<HomePage />)} />
-        <Route path={ROUTES.transactionInput} element={authRoute(<InputAmount />)} />
-        <Route path={ROUTES.transactions} element={authRoute(<AccountBook />)} />
-        <Route path={ROUTES.transactionEdit} element={authRoute(<InputAmount />)} />
-        <Route path={ROUTES.profiles} element={authRoute(<EditProfiles />)} />
-      </Route>
-    </Routes>
+    <App />
   </Router>,
-);
+)
 
-reportWebVitals();
+reportWebVitals()
